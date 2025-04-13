0603f3258ffa2aa3b43911080a9a9e8963b49f49
Codeurs Passionnés <codeurspassionnes@Mac-mini-de-Yann.local>
2025-04-13 08:32:21 -0400

    inertia + vue + adminlte installed

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from "@vitejs/plugin-vue"
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.js'],
+            refresh: true,
+        }),
+        vue()
+    ],
+    resolve: {
+        alias: {
+            $: 'jquery',
+            jquery: 'jquery',
+        },
+    },
+});
